f3b8cf7315b6e58e0037d18e6b8d94abd5ba03ec
NTUT110441524 <95857769+NTUT110441524@users.noreply.github.com>
2021-12-23 21:15:38 +0800

    Update test1223.py

--- test1223.py
+++ test1223.py
@@ -1,44 +1,31 @@
-import random
+#1A2B game
 import streamlit as st
+import random
 
-ranlist = []
-def generate(lst):
-    while len(lst) < 4:
-        lst.append(randint(1,9))
-        if len(lst) > 1:
-            x = 0
-            while x < len(lst)-1:
-                if lst[x] == lst[len(lst)-1]:
-                    del lst[len(lst)-1]
-                else:
-                    x += 1
-    final_ans = ""
-    for i in range(0,len(lst)):
-        final_ans = final_ans + str(lst[i])
-    return final_ans
-def gamer():
-    print("歡迎來到1A2B，密碼已經產生，請在四次之內猜中正確答案")
-    finans = generate(ranlist)
-    starter = True
-    counter = 1
-    while starter:
-        if counter == 4:
-            st.write("Game Over!!")
-            starter = False
-        while counter < 5:
-            result = 0
-            answer = input("請輸入你的答案：")
-            for i in range(0,len(finans)):
-                for j in range(0,len(finans)):
-                    if answer[i] == finans[j]:
-                        if i == j:
-                            result += 10
-                        else:
-                            result += 1
-            #print
-            st.write(str(int(result/10)) + 'A' + str(result%10) + 'B')
-            counter += 1
-            if result == 40:
-                print("恭喜你，答案就是" + str(finans))
-                starter = False
-gamer()
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+answer=random.shuffle(items)
+answer=''
+a_count=0 # initial A count
+b_count=0 # initial B count
+
+for i in range(4):
+    answer+=str(items[i])
+while(True):
+    number=st.text_input('請輸入數字： ')
+    st.write(answer)
+    if not number.isdigit():  #cheak all input is digit
+        pass
+    else:
+        if number==answer:
+            st.write('很棒你答對了')
+            break
+        for i in range(4):
+            for j in range(4):
+                if i==j and number[i]==answer[j]:
+                    a_count+=1
+                elif number[i]==answer[j]:
+                    b_count+=1
+        st.write('{0}A{1}B'.format(a_count,b_count))
+        a_count=0
+        b_count=0
+        
